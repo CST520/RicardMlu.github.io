f8d60cb7a6e214e068eb8036e2e2b420f3aa3159
CST520 <1975492821@qq.com>
2022-02-23 09:08:38 +0800

    first add

---
+++ H5/coment01.java
@@ -0,0 +1,7 @@
+public class coment01 {
+	public static void main(String[] args){
+		int n1 = 4;
+		int n2 = 6;
+		System.out.println(n1 * n1);
+	}	
+}
---
+++ H5/hello.java
@@ -0,0 +1,6 @@
+public class hello {
+	public static vod main(String[] args){
+		//System.out.println("woaini,weileniwoyaoxuejava\n");
+		System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+	}	
+}
